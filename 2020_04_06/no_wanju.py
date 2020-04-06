707486dc57791d73b51c39e3a720fb82aa5c00cb
Ryan Jung <jjs1005k@naver.com>
2020-04-06 12:54:57 +0000

    add

---
+++ 2020_04_06/no_wanju.py
@@ -0,0 +1,17 @@
+def solution(participant, completion):
+    if 1 > len(participant) or len(participant) > 100000:
+        return 0
+    answer = ''
+    return answer
+
+#참가자 수는 1명 이상 100,000명 이하
+#이름은 알파벳 소문자 1 <= X <= 20
+#동명이인 존재할 수 있음
+participant = ["leo", "kiki", "eden"]
+
+#len(completion) == len(participant)-1
+completion = []
+
+for x in participant:
+    if 
+#print(solution(participant, completion))
